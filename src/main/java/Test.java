29859cdf4d06ab6fcd7e5c8000e20c57f1996ed8
GLOBBIMUS <striker1342566@gmail.com>
2017-11-09 22:39:18 -0600

    Change argument names.

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -4,8 +4,7 @@ public class Test {
     public static void main(String[] args) throws IOException {
         //Make instance of the converter
         Converter Converter = new Converter();
-
         //To convert edn file call the convert method which takes two arguments two arguments: sourceFile and destination for the output files.
-        Converter.convert("your_edn_file", "target");
+        Converter.convert("sourceFile" ,"destinationDirectory");
     }
 }
